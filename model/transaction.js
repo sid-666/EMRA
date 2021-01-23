a07fd6842385bfbed4567debf282470d76d95347
Seance <siddharth.dhananjay@gmail.com>
2021-01-23 15:58:10 +1100

    added another property to the transaction schema

---
+++ model/transaction.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const transactionSchema = new Schema(
+    {
+        type: {
+            type: String,
+            trim: true,
+            required: "Enter transaction type"
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: "Enter a name for transaction"
+        },
+        value: {
+            type: Number,
+            required: "Enter an amount"
+        },
+        date: {
+            type: Date,
+            default: Date.now
+        }
+    }
+);
+
+const Transaction = mongoose.model("Transaction", transactionSchema);
+
+module.exports = Transaction;
