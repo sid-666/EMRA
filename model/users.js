01f6d98f204e9cfccca7b4b6786a0f6951a67add
Seance <siddharth.dhananjay@gmail.com>
2021-01-23 16:00:06 +1100

    Made users schema as well

---
+++ model/users.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    unique: true
+  },
+  transaction: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Transaction"
+    }
+  ]
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
