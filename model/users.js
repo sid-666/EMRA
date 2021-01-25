1bc020c66d83993a0e30ac246e002e8965b616ae
Seance <siddharth.dhananjay@gmail.com>
2021-01-25 19:52:08 +1100

    Added password for middleware

--- model/users.js
+++ model/users.js
@@ -3,16 +3,20 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  name: {
-    type: String,
-    unique: true
-  },
-  transaction: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Transaction"
-    }
-  ]
+    name: {
+        type: String,
+        unique: true
+    },
+    password: {
+        type: String,
+        unique: true
+    },
+    transaction: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Transaction"
+        }
+    ]
 });
 
 const User = mongoose.model("User", UserSchema);
