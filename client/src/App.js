c5a0611dd64b641cf14c86c9fa783fc7c0262428
Seance <siddharth.dhananjay@gmail.com>
2021-01-27 16:20:18 +1100

    got client side code

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,80 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Search from "./pages/Search";
-import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
-import Footer from "./components/Footer"
-import Saved from "./pages/Saved";
-
-
+  
+import React, { useState } from "react";
+import "./App.css";
+import Axios from "axios";
 
 function App() {
+  const [registerUsername, setRegisterUsername] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [loginUsername, setLoginUsername] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const [data, setData] = useState(null);
+  const register = () => {
+    Axios({
+      method: "POST",
+      data: {
+        username: registerUsername,
+        password: registerPassword,
+      },
+      withCredentials: true,
+      url: "http://localhost:4000/register",
+    }).then((res) => console.log(res));
+  };
+  const login = () => {
+    Axios({
+      method: "POST",
+      data: {
+        username: loginUsername,
+        password: loginPassword,
+      },
+      withCredentials: true,
+      url: "http://localhost:4000/login",
+    }).then((res) => console.log(res));
+  };
+  const getUser = () => {
+    Axios({
+      method: "GET",
+      withCredentials: true,
+      url: "http://localhost:4000/user",
+    }).then((res) => {
+      setData(res.data);
+      console.log(res.data);
+    });
+  };
   return (
-    <Router>
+    <div className="App">
       <div>
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={Search} />
-          <Route exact path="/saved" component={Saved} />
-          <Route exact path="/saved/:id" component={Saved} />
-          <Route component={NoMatch} />
-        </Switch>
-        <Footer />
+        <h1>Register</h1>
+        <input
+          placeholder="username"
+          onChange={(e) => setRegisterUsername(e.target.value)}
+        />
+        <input
+          placeholder="password"
+          onChange={(e) => setRegisterPassword(e.target.value)}
+        />
+        <button onClick={register}>Submit</button>
       </div>
-    </Router>
+
+      <div>
+        <h1>Login</h1>
+        <input
+          placeholder="username"
+          onChange={(e) => setLoginUsername(e.target.value)}
+        />
+        <input
+          placeholder="password"
+          onChange={(e) => setLoginPassword(e.target.value)}
+        />
+        <button onClick={login}>Submit</button>
+      </div>
+
+      <div>
+        <h1>Get User</h1>
+        <button onClick={getUser}>Submit</button>
+        {data ? <h1>Welcome Back {data.username}</h1> : null}
+      </div>
+    </div>
   );
 }
 
