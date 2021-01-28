7200dee4f784a230c6b0e6f8425ae9b890229707
Seance <siddharth.dhananjay@gmail.com>
2021-01-28 15:58:56 +1100

    correction srver side

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+const path = require('path')
 const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
@@ -5,11 +7,43 @@ const passport = require("passport");
 const passportLocal = require("passport-local").Strategy;
 const cookieParser = require("cookie-parser");
 const bcrypt = require("bcryptjs");
+const http = require("http");
 const session = require("express-session");
+const flash = require('connect-flash');
 const bodyParser = require("body-parser");
 const app = express();
-const User = require("./model/user");
+const User = require("./model/users");
+const PORT = process.env.PORT || 3001;
+const router = require("./routes")
 //----------------------------------------- END OF IMPORTS---------------------------------------------------
+
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+app.use(
+    cors({
+        origin: "http://localhost:3000", // <-- location of the react app were connecting to
+        credentials: true,
+    })
+);
+app.use(
+    session({
+        secret: "secretcode",
+        resave: false,
+        saveUninitialized: false,
+    })
+);
+app.use(cookieParser("secretcode"));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+require("./config/passport")(passport);
+
+app.use(router);
 mongoose.connect(
     process.env.MONGODB_URI,
     {
@@ -20,23 +54,10 @@ mongoose.connect(
     }
 );
 
-// Middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: "http://localhost:3000", // <-- location of the react app were connecting to
-    credentials: true,
-  })
-);
-app.use(
-  session({
-    secret: "secretcode",
-    resave: true,
-    saveUninitialized: true,
-  })
-);
-app.use(cookieParser("secretcode"));
-app.use(passport.initialize());
-app.use(passport.session());
-require("./passportConfig")(passport);
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+})
+const httpServer = http.createServer(app);
+httpServer.listen(PORT, function () {
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
