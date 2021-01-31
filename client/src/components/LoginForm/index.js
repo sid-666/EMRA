f550442cac43eb1c4ec71c20a659369f2e266fe1
Seance <siddharth.dhananjay@gmail.com>
2021-01-31 21:38:00 +1100

    did skeleton template for login component

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -1,21 +1,22 @@
-import React, {useState} from "react";
+import React from "react";
 import "./style.css";
 const LoginForm = props => {
    return (
-    <div>
+    <div className>
         <h1>Login</h1>
         <input
         placeholder="username"
-        value={loginUsername}
+        value={props.loginUsername}
         onChange={props.setUsername}
         />
         <input
         type="password"
         placeholder="password"
-        value={loginPassword}
+        value={props.loginPassword}
         onChange={props.setPassword}
         />
         <button onClick={props.login}>Submit</button>
     </div>  
    )   
 }
+export default LoginForm
