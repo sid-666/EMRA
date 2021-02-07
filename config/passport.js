5fb136a1ef51cf79e48aa99d99f7d997218cf23b
Seance <siddharth.dhananjay@gmail.com>
2021-02-08 05:51:27 +1100

    debugging done to identify why authentication of password was not working

--- config/passport.js
+++ config/passport.js
@@ -1,15 +1,18 @@
 const { User } = require("../model");
-const bcrypt = require("bcryptjs");
 const localStrategy = require("passport-local").Strategy;
 
 module.exports = function (passport) {
     passport.use(
         new localStrategy((username, password, done) => {
+            console.log(password);
+            console.log(username)
+            console.log(done);
             User.findOne({
                 where: {
                     username: username
                 }
-            }).then(function(dbUser) {
+            }).then(function (dbUser) {
+                console.log(dbUser.get())
                 // If there's no user with the given email
                 if (!dbUser) {
                     return done(null, false, {
@@ -17,22 +20,22 @@ module.exports = function (passport) {
                     });
                 }
                 // If there is a user with the given email, but the password the user gives us is incorrect
-                else if (!dbUser.validPassword(password)) {
+                if (!dbUser.validPassword(password)) {
                     return done(null, false, {
                         message: "Incorrect password."
                     });
                 }
                 // If none of the above, return the user
-                return done(null, dbUser);
-            });           
+                return done(null, dbUser.get());
+            });
         })
     );
 
-    passport.serializeUser(function(user, cb) {
+    passport.serializeUser(function (user, cb) {
         cb(null, user);
     });
-    
-    passport.deserializeUser(function(obj, cb) {
+
+    passport.deserializeUser(function (obj, cb) {
         cb(null, obj);
     });
 };
