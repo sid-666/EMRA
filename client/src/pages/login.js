586abc9d5e2e9dc2de671774afadafb36d2e2d78
Seance <siddharth.dhananjay@gmail.com>
2021-02-08 20:53:29 +1100

    changed login ui

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -2,6 +2,8 @@ import React, { useState, useContext } from "react";
 import { useHistory } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
 import Axios from "axios";
+import { Grid, Paper, Avatar, TextField, Button } from '@material-ui/core'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 
 function Login() {
     const [loginUsername, setLoginUsername] = useState("");
@@ -29,22 +31,24 @@ function Login() {
             console.log(err)
         }
     };
+    const paperStyle = { padding: 20, height: '70vh', width: 280, margin: "20px auto" }
+    const avatarStyle = { backgroundColor: '#1bbd7e' }
+    const btnstyle = { margin: '8px 0' }
+    const divider = { margin: '20px' }
     return (
-        <div>
-            <h1>Login</h1>
-            <input
-                placeholder="username"
-                value={loginUsername}
-                onChange={(e) => setLoginUsername(e.target.value)}
-            />
-            <input
-                type="password"
-                placeholder="password"
-                value={loginPassword}
-                onChange={(e) => setLoginPassword(e.target.value)}
-            />
-            <button onClick={login}>Submit</button>
-        </div>
+        <Grid>
+            <Paper elevation={10} style={paperStyle} spacing={3}>
+                <Grid align='center'>
+                    <Avatar style={avatarStyle}><LockOutlinedIcon /></Avatar>
+                    <h2>Log in</h2>
+                </Grid>
+                <TextField label='Username' onChange= {(e)=> {setLoginUsername(e.target.value)}} placeholder='Enter username' fullWidth required />
+                <TextField label='Password' onChange= {(e)=> {setLoginPassword(e.target.value)}} placeholder='Enter password' type='password' fullWidth required />
+                <div style={divider}></div>
+                <Button type='submit' onClick= {login}color='primary' variant="contained" style={btnstyle} fullWidth>Sign in</Button>
+            </Paper>
+        </Grid>
     )
+
 }
 export default Login
